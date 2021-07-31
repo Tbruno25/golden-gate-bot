a015e5f600e0b61c6a0c15ea237ea71093a4ab76
TJ <tbruno25@gmail.com>
2021-07-31 13:13:49 -0700

    split environment variables into own file

--- app.py
+++ app.py
@@ -6,24 +6,10 @@ from redis import StrictRedis
 import json
 import os
 
+from env_vars import *
+
 
 subreddit = "sanfrancisco"
-
-# Get Reddit keys
-reddit_credentials_json = os.getenv("REDDIT_APPLICATION_JSON")
-reddit_credentials = json.loads(reddit_credentials_json, strict=False)
-
-# Get Google keys
-google_credentials_json = os.getenv("GOOGLE_APPLICATION_JSON")
-google_credentials = json.loads(google_credentials_json, strict=False)
-with open("google-key.json", "w") as file:
-    json.dump(google_credentials, file)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google-key.json"
-
-# Get Database keys
-database_host = os.getenv("QOVERY_REDIS_ZBBCDA9B9_HOST")
-database_port = os.getenv("QOVERY_REDIS_ZBBCDA9B9_PORT")
-database_password = os.getenv("QOVERY_REDIS_ZBBCDA9B9_PASSWORD")
 
 
 class Bot:
